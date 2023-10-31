875e53d41365172c7814b682ca12ac38911b5924
Park Hangyeol <127859370+Hangyeol0516@users.noreply.github.com>
2023-10-31 18:26:31 +0900

    Delete src directory

--- src/blackBox/_12_Inheritance.java
+++
@@ -1,9 +0,0 @@
-package blackBox;
-
-public class _12_Inheritance {
-    public static void main(String[] args) {
-        // 상속
-        // 부모로 부터 재산을 물려받아 자식이 사용하는 것
-        // 부모 클래스에서 제공되는 모든 것들을 자식 클래스에서 갖다 쓴다.
-    }
-}
